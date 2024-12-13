2312658a57989a8bdad2f1966af91550e9497dd3
LucasCruz <lca.snf21@uea.edu.br>
2024-12-13 05:41:25 -0400

    update db logic

--- backend/data_modules/models.py
+++ backend/data_modules/models.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, Integer, String, TIMESTAMP
+from sqlalchemy import Column, Integer, String, TIMESTAMP, Text, DateTime
 from sqlalchemy.sql import func
 from .config import Base
-
+import datetime
 class User(Base):
     __tablename__ = "users"
     
@@ -9,3 +9,11 @@ class User(Base):
     username = Column(String(50), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
     created_at = Column(TIMESTAMP, server_default=func.now())
+
+class Note(Base):
+    __tablename__ = "notes"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255), nullable=False)
+    content = Column(Text, nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.now(datetime.timezone.utc))
+    updated_at = Column(DateTime, default=datetime.datetime.now(datetime.timezone.utc))
--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,9 @@ from pydantic import BaseModel
 from typing import Any
 import os
 from data_modules.config import SessionLocal, engine
-from data_modules.models import User
+from data_modules.models import User, Note
 from sqlalchemy.orm import Session
+import datetime
 
 # Diretório onde os arquivos serão salvos
 UPLOAD_DIRECTORY = "uploads"
@@ -55,4 +56,65 @@ async def post_one( user: str = Form(...),
         "message": "Dados recebidos com sucesso!",
         "user": user,
         "photo_filename": photo.filename
+    }
+
+@app.post("/login")
+async def login( user: str = Form(...), password: str = Form(...)):
+    return {
+        "message": "login",
+    }
+
+
+
+@app.post("/note")
+async def create_note( 
+    note_title: str = Form(...), 
+    db:Session = Depends(get_db) ):
+    db_note = Note( title=note_title, content="" )
+    db.add(db_note)
+    db.commit()
+    db.refresh(db_note)
+    return {
+        "message": "login",
+    }
+
+@app.get("/note")
+async def get_all_notes(
+    db:Session = Depends(get_db)
+    ):
+
+    notes = db.query(Note).all()
+    return notes
+
+@app.get("/note/{note_id}")
+async def get_note_by_id(
+    note_id: int,
+    db:Session = Depends(get_db)
+    ):
+    note = db.query(Note).filter(Note.id == note_id).first()
+    if not note:
+        raise HTTPException(status_code=404, detail="Note not found")
+    return note
+
+
+@app.put("/note/{note_id}")
+async def update_task(
+    note_id: int,
+    note_content: str = Form(...),
+    db:Session = Depends(get_db)):
+
+    note = db.query(Note).filter(Note.id == note_id).first()
+    
+    if not note:
+        # Lança exceção se não encontrar o registro
+        raise HTTPException(status_code=404, detail="Note not found")
+    
+    note.content = note_content
+    note.updated_at = datetime.datetime.now(datetime.timezone.utc)  # Atualiza a data de modificação
+    
+    # Commit as mudanças no banco
+    db.commit()
+    db.refresh(note)
+    return {
+        "message": "login",
     }
