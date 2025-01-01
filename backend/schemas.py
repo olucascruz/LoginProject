5ff1ebd404c6bd15d29c3e1da7b753f6b7b4347d
LucasCruz <lca.snf21@uea.edu.br>
2025-01-01 19:19:02 -0400

    feat: add token and tokenData schemas

--- backend/schemas.py
+++ backend/schemas.py
@@ -15,3 +15,11 @@ class NoteSchema(BaseModel):
     created_at:datetime
     updated_at:datetime
 
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
