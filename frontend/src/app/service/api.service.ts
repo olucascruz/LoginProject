5392ba3c89215ee04c2bb4fef8785d956442984f
LucasCruz <lca.snf21@uea.edu.br>
2025-01-03 13:04:45 -0400

    feat: create functions with auth to control notes

--- frontend/src/app/service/api.service.ts
+++ frontend/src/app/service/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 export interface RegisterResponse {
@@ -11,6 +11,13 @@ export interface RegisterResponse {
   };
 }
 
+export interface NoteResponse {
+  id: number;
+  title: string;
+  content: string;
+  created_at: string;
+  updated_at: string;
+}
 
 
 
@@ -22,15 +29,57 @@ export class ApiService {
   // URL do endpoint para onde os dados serão enviados
   private apiUrl = 'http://127.0.0.1:8000/';  // Substitua com o URL real
 
+  tryGetTokenAccess(){
+    const token = localStorage.getItem("accessToken");
+    if(!token){
+      window.location.href = "/";
+      return null;
+    }
+    return token
+  }
+
+  getHeaders(): HttpHeaders {
+    const token = this.tryGetTokenAccess()
+    if(token==null){
+      throw new Error("Token de acesso não encontrado. Faça login novamente.");
+    }
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+  }
   constructor(private http: HttpClient) {}
 
   register(data: FormData): Observable<RegisterResponse> {
     const url = this.apiUrl+'register'
-    return this.http.post<RegisterResponse>(this.apiUrl, data);
+    return this.http.post<RegisterResponse>(url, data);
   }
 
-  createNote(formTitle:str): Observable<any>{
-    return null
+  login(data: FormData): Observable<any> {
+    const url = this.apiUrl+'login'
+    return this.http.post<any>(url, data);
   }
 
+  createNote(formTitle:FormData): Observable<any>{
+    const url = this.apiUrl+'note'
+    const headers = this.getHeaders()
+    return this.http.post<any>(url, formTitle, {headers});
+  }
+
+  updateNote(id:number, dataNote:FormData): Observable<any>{
+    const url = this.apiUrl+'note/'+id.toString()
+    const headers = this.getHeaders()
+    return this.http.put<any>(url, dataNote, {headers});
+  }
+
+  getNotes(): Observable<NoteResponse[]>{
+    const url = this.apiUrl+'note'
+    const headers = this.getHeaders()
+    return this.http.get<NoteResponse[]>(url, {headers});
+  }
+
+  getNoteById(id: number): Observable<NoteResponse> {
+    const url = `${this.apiUrl}note/${id}`;
+    const headers = this.getHeaders()
+    return this.http.get<NoteResponse>(url, {headers});
+  }
 }
