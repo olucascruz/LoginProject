2aa2d15f0905af2cf4539fc184c15007ae838783
LucasCruz <lca.snf21@uea.edu.br>
2025-01-16 00:04:02 -0400

    feat: update routes

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta, timezone
-from http import HTTPStatus
 from typing import Annotated
 
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -12,11 +11,11 @@ from passlib.context import CryptContext
 from backend.data_modules.database import get_session
 from backend.data_modules.models import User
 from jwt.exceptions import InvalidTokenError
-from schemas import TokenData
-
+from schemas import TokenData, Token
 
 SECRET_KEY = "hello world"
 ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 180
 # oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 router = APIRouter(prefix='/auth', tags=['auth'])
 
@@ -76,3 +75,23 @@ async def get_current_user(
     if user is None:
         raise credentials_exception
     return user
+
+
+@router.post("/login", status_code=200)
+async def login(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db:DbSession
+    ):
+    
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"})
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+
+    return Token(access_token=access_token, token_type="bearer")
---
+++ backend/routers/notes.py
@@ -0,0 +1,96 @@
+from backend.data_modules.database import get_db
+from backend.data_modules.models import User, Note
+from sqlalchemy.orm import Session
+import datetime
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError, DataError
+from fastapi.responses import JSONResponse
+from schemas import UserSchema, UserPublic, NoteSchema
+from typing import Annotated
+from fastapi import APIRouter, Form, Depends, HTTPException
+from auth import get_current_user
+from datetime import timezone
+
+router = APIRouter(prefix='/note', tags=['users'])
+
+@router.post("/note", status_code=201, response_model=NoteSchema)
+async def create_note( 
+    current_user: Annotated[User, Depends(get_current_user)],
+    note_title: str = Form(...),
+    db:Session = Depends(get_db)
+    ):
+
+    db_note = Note(user_id=current_user.id, title=note_title, content="")
+    db.add(db_note)
+    db.commit()
+    db.refresh(db_note)
+
+    return db_note
+
+@router.get("/note", response_model=list[NoteSchema])
+async def get_all_notes(
+    current_user: Annotated[User, Depends(get_current_user)],
+    db:Session = Depends(get_db),
+    ):
+    notes = db.query(Note).filter(Note.user_id == current_user.id).all()
+
+    
+    return notes
+
+@router.get("/note/{note_id}", response_model=NoteSchema)
+async def get_note_by_id(
+    current_user: Annotated[User, Depends(get_current_user)],
+    note_id: int,
+    db:Session = Depends(get_db)
+    ):
+    
+    note = db.query(Note).filter(
+        Note.id == note_id, 
+        Note.user_id==current_user.id).first()
+    if not note:
+        raise HTTPException(status_code=404, detail="Note not found")
+    return note
+
+
+@router.put("/note/{note_id}")
+async def update_note(
+    current_user: Annotated[User, Depends(get_current_user)],
+    note_id: int,
+    note_title: str = Form(...),
+    note_content: str = Form(...),
+    db:Session = Depends(get_db)):
+
+    note = db.query(Note).filter(Note.id == note_id, Note.user_id == current_user.id).first()
+    
+    if not note:
+        # Lança exceção se não encontrar o registro
+        raise HTTPException(status_code=404, detail="Note not found")
+    
+    note.title = note_title
+    note.content = note_content
+    note.updated_at = datetime.datetime.now(timezone.utc) 
+    
+    # Commit as mudanças no banco
+    db.commit()
+    db.refresh(note)
+    return note
+
+
+@router.delete("/note/{note_id}")
+async def delete_note(
+    current_user: Annotated[User, Depends(get_current_user)],
+    note_id: int,
+    db: Session = Depends(get_db)):
+    # Busca a nota no banco de dados
+    note = db.query(Note).filter(Note.id == note_id, Note.user_id == current_user.id).first()
+    
+    if not note:
+        # Lança exceção se não encontrar o registro
+        raise HTTPException(status_code=404, detail="Note not found")
+    
+    # Remove a nota do banco de dados
+    db.delete(note)
+    db.commit()
+    
+    return {
+        "message": "Note deleted successfully"
+    }
